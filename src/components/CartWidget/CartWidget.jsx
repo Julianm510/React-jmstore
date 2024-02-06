dd54cbd0a4c741731efbdb06e836d3a70596661e
Julian <julianmuriel510@gmail.com>
2024-02-06 16:43:10 -0300

    primera pre-entrega

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,14 +1,19 @@
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import './cartWidget.css';
 
-function CartWidget(props) {
-    return (
-      <div className='containerCart'>
-        <ShoppingCartIcon className='cartIcon' onClick={(event){handleClick(event);}}/>
-        <span className='cartNumber'>{props.number}</span>
-      </div>
-    )
-  }
+function CartWidget({number, handleClick}) {
+  return (
+    <div className="containerCart">
+      <ShoppingCartIcon
+       className="cartIcon"
+       onClick={(event) => {
+        handleClick(event);
+         }}
+          />
+      <span className="cartNumber">{number}</span>
+    </div>
+  )
+}
 
 export default CartWidget
 
